79cc19c78c1a7d5978c0b62ece6ea31409e50f2a
sameer666 <singh.sameer9415@gmail.com>
2024-02-08 20:00:24 +0530

    readme

--- AWS_WORKSHOP/pod11/Development/outputs.tf
+++ AWS_WORKSHOP/pod11/Development/outputs.tf
@@ -1,3 +1,11 @@
 output vpc_id {
     value = module.network.vpc_id
 }
+
+output ftd {
+    value = module.instance.instance_private_ip
+}
+
+output fmc {
+    value = module.instance.fmc_instance_private_ip
+}
